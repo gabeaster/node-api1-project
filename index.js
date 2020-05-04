546de59cf933c0bf73547c5d79fe88bf74aef4b5
gabeaster <gab.easter@live.com>
2020-05-04 17:59:08 -0400

    post request
    edited post request to have better logic, status 400, 201, and 500 included

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express"); //CommonJS Module
-const { v4: uuidv4 } = require("uuid");
+const db = require("./data/db");
 const server = express();
 server.use(express.json()); //teaches express how to read json
 server.get("/", (req, res) => {
@@ -21,26 +21,66 @@ let users = [
 server.get("/api/users", function (req, res) {
   res.json(users);
 });
+
 //post request /api/users creates a user using the information sent inside the request body
 server.post("/api/users", function (req, res) {
-  const userInformation = req.body;
-  users.push(userInformation);
-  res.status(200).json(userInformation);
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    const newUser = { id: Date.now(), name, bio };
+    db.insert(newUser)
+      .then((addedUser) => {
+        res.status(201).json({ newUser });
+      })
+      .catch((err) =>
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the user to the database. ",
+        })
+      );
+  }
 });
-//get /api/users/:id returns the user object with the specified id
-server.get("/api/users/:id", function (req, res) {
-  const id = req.params.id;
-  res.status(200).json("user" + req.params.id);
-});
-//delete /api/users/:id removes the user with the specified id and returns the deleted user
-server.delete("/api/users/:id", function (req, res) {
-  const id = req.params.id;
 
-  //find the user on the array and remove it
-  users = users.filter((user) => user.id != id);
-  res.status(200).json(users);
-});
+//get /api/users/:id returns the user object with the specified id
+// server.get("/api/users/:id", function (req, res) {
+//   const id = req.params.id;
+//   if (id === undefined) {
+//     res
+//       .status(404)
+//       .json({ message: "The user with the specified ID does not exist." });
+//   } else if (id != req.params.id) {
+//     res
+//       .status(500)
+//       .json({ errorMessage: "The user information could not be retrieved." });
+//   }
+//   res.status(200).json("user" + req.params.id);
+// });
+
+//delete /api/users/:id removes the user with the specified id and returns the deleted user
+// server.delete("/api/users/:id", function (req, res) {
+//   const id = req.params.id;
+//   if (id === undefined) {
+//     res
+//       .status(404)
+//       .json({ message: "The user with the specified ID does not exist." });
+//   } else if (id != req.params.id) {
+//     res
+//       .status(500)
+//       .json({ errorMessage: "The user information could not be retrieved." });
+//   } else users = users.filter((user) => user.id != id);
+//   res.status(200).json(users);
+// });
 
 //patch /api/users/:id updates the user with the specified id using data form the request body. Returns the modified user.
+// server.patch("/api/users/:id", function (req, res) {
+//   const id = req.params.id;
+//   updatedUser = users.filter((user) => user.id == id);
+//   const userInformation = req.body;
+//   users.push(userInformation);
+//   res.status(200).json(users);
+// });
 
 server.listen(9000, () => console.log("/ API IS UP /"));
