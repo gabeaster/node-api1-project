99969822e52ed25b6d310dc54af6f7927488a5bd
gabeaster <gab.easter@live.com>
2020-05-04 17:17:06 -0400

    .delete users/:id
    i setup server.delete to the /api/users/:id endpoint. I created a const id that equals req.paramsid. Then, I filtered through users to return all users that do not match the provided id. I returned a response with status(200) and the json(users).

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ server.use(express.json()); //teaches express how to read json
 server.get("/", (req, res) => {
   res.json({ api: "up and running!" });
 });
+//User Schema:
+// {
+//   id: "a_unique_id", // hint: use the shortid npm package to generate it
+//   name: "Jane Doe", // String, required
+//   bio: "Not Tarzan's Wife, another Jane",  // String, required
+// }
 let users = [
   {
     id: 1,
@@ -22,13 +28,16 @@ server.post("/api/users", function (req, res) {
   res.status(200).json(userInformation);
 });
 //get /api/users/:id returns the user object with the specified id
+server.get("/api/users/:id");
 //delete /api/users/:id removes the user with the specified id and returns the deleted user
+server.delete("/api/users/:id", function (req, res) {
+  const id = req.params.id;
+
+  //find the user on the array and remove it
+  users = users.filter((user) => user.id != id);
+  res.status(200).json(users);
+});
+
 //patch /api/users/:id updates the user with the specified id using data form the request body. Returns the modified user.
 
-//User Schema:
-// {
-//   id: "a_unique_id", // hint: use the shortid npm package to generate it
-//   name: "Jane Doe", // String, required
-//   bio: "Not Tarzan's Wife, another Jane",  // String, required
-// }
 server.listen(9000, () => console.log("/ API IS UP /"));
