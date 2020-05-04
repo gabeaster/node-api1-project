a3ccd493691bb3f4f3576a8be9f896b3e0efd2b2
gabeaster <gab.easter@live.com>
2020-05-04 17:05:40 -0400

    post request /api/users
    moved users array to global, setup cpost for userInformation. pushed userInformation to users array and return res.status(200).json(userInformation). successful in postman

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require("express"); //CommonJS Module
+const { v4: uuidv4 } = require("uuid");
 const server = express();
-
+server.use(express.json()); //teaches express how to read json
 server.get("/", (req, res) => {
   res.json({ api: "up and running!" });
 });
-
+let users = [
+  {
+    id: 1,
+    name: "array of users",
+  },
+];
 //get /api/users returns an array of users
 server.get("/api/users", function (req, res) {
-  const users = [
-    {
-      id: 1,
-      name: "array of users",
-    },
-  ];
   res.json(users);
 });
 //post request /api/users creates a user using the information sent inside the request body
 server.post("/api/users", function (req, res) {
   const userInformation = req.body;
-  //
+  users.push(userInformation);
+  res.status(200).json(userInformation);
 });
 //get /api/users/:id returns the user object with the specified id
 //delete /api/users/:id removes the user with the specified id and returns the deleted user
